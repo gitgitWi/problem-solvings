cd61ccc8ad3a85885eb34d89d84ff2dd95f8c706
gitgitwi <gitgitwi@gmail.com>
2024-05-16 13:23:26 +0000

    feat(vitest) include js files

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
-    include: ['./problems/**/*.test.ts'],
+    include: ['./problems/**/*.test.{ts,js}'],
   },
 });
