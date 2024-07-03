50ef3599eafab6850b5a24d76945649404e614fb
gitgitWi <wiii@kakao.com>
2024-07-04 08:46:35 +0900

    setup test cases - 프로그래머스 12914, 12985

---
+++ problems/programmers/lv2/12914.test.js
@@ -0,0 +1,21 @@
+import { describe, test, expect } from "vitest";
+
+describe("12914. 멀리 뛰기", () => {
+  /**
+   * @see {@link https://school.programmers.co.kr/learn/courses/30/lessons/12914}
+   * @param {number} n
+   * @returns {number}
+   */
+  function solution(n) {
+    let answer = 0;
+    return answer;
+  }
+
+  test("TC1", () => {
+    expect(solution(4)).toEqual(5);
+  });
+
+  test("TC2", () => {
+    expect(solution(3)).toEqual(3);
+  });
+});
---
+++ problems/programmers/lv2/12985.test.js
@@ -0,0 +1,21 @@
+import { describe, test, expect } from "vitest";
+
+describe("12985. 예상 대진표", () => {
+  /**
+   * @see {@link https://school.programmers.co.kr/learn/courses/30/lessons/12985}
+   *
+   * @param {number} n
+   * @param {number} a
+   * @param {number} b
+   * @returns {number}
+   */
+  function solution(n, a, b) {
+    let answer = 0;
+
+    return answer;
+  }
+
+  test("TC1", () => {
+    expect(solution(8, 4, 7)).toEqual(3);
+  });
+});
